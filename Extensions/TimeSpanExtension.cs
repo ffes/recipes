1a62f3d5d780f5e749a553453b22a6cad4138085
Frank Fesevur <fesevur@gmail.com>
2021-01-24 22:38:02 +0100

    Rewrite the TimeSpan Extension ToReadableString()

--- Extensions/TimeSpanExtension.cs
+++ Extensions/TimeSpanExtension.cs
@@ -6,19 +6,20 @@ namespace Recipes
 	{
 		public static string ToReadableString(this TimeSpan span)
 		{
-			string formatted = string.Format("{0}{1}{2}{3}",
-				span.Duration().Days > 0 ? string.Format("{0:0} day{1}, ", span.Days, span.Days == 1 ? string.Empty : "s") : string.Empty,
-				span.Duration().Hours > 0 ? string.Format("{0:0} hour{1}, ", span.Hours, span.Hours == 1 ? string.Empty : "s") : string.Empty,
-				span.Duration().Minutes > 0 ? string.Format("{0:0} minute{1}, ", span.Minutes, span.Minutes == 1 ? string.Empty : "s") : string.Empty,
-				span.Duration().Seconds > 0 ? string.Format("{0:0} second{1}", span.Seconds, span.Seconds == 1 ? string.Empty : "s") : string.Empty);
+			string readable = "";
+			if (span.Hours > 0)
+			{
+				readable += $"{span.Hours} uur";
+			}
 
-			if (formatted.EndsWith(", "))
-				formatted = formatted[0..^2];
+			if (span.Minutes > 0)
+			{
+				if (readable.Length > 0)
+					readable += ", ";
+				readable += $"{span.Minutes} " + (span.Minutes == 1 ? "minuut" : "minuten");
+			}
 
-			if (string.IsNullOrEmpty(formatted))
-				return "0 seconds";
-
-			return formatted;
+			return readable;
 		}
 	}
 }
